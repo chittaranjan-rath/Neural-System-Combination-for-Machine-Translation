fd64083ffa8bacdefea153617f9e007010f76082
chittaranjan-rath <42587032+chittaranjan-rath@users.noreply.github.com>
2020-05-06 22:14:05 +0530

    Delete train.cmd

--- train.cmd
+++
@@ -1,6 +0,0 @@
-
-nohup nice ~/mosesdecoder/scripts/training/train-model.perl -root-dir train
--corpus ~/corpus/en-hi.clean
--f fr -e en -alignment grow-diag-final-and -reordering msd-bidirectional-fe
--lm 0:3:$HOME/lm/en-hi.cor
--external-bin-dir ~/mosesdecoder/tools >& training.out &
