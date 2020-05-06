6cd058ed2cbc54029398a379c57207b9170364b2
chittaranjan-rath <42587032+chittaranjan-rath@users.noreply.github.com>
2020-05-06 22:13:58 +0530

    Delete tokenization.cmd

--- tokenization.cmd
+++
@@ -1,8 +0,0 @@
-~/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en \
-< ~/corpus/training/hi-en.cor
-\
-> ~/corpus/news-commentary-v8.fr-en.tok.en
-~/mosesdecoder/scripts/tokenizer/tokenizer.perl -l fr \
-< ~/corpus/training/hi-en.cor
-\
-> ~/corpus/hi-en.tok.fr
