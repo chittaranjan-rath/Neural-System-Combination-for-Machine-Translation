6f028b0631c22f1bc12c1bc5bbe4f107622256d4
chittaranjan-rath <42587032+chittaranjan-rath@users.noreply.github.com>
2020-05-06 22:14:15 +0530

    Delete truecasing.cmd

--- truecasing.cmd
+++
@@ -1,8 +0,0 @@
-~/mosesdecoder/scripts/recaser/train-truecaser.perl \
---model ~/corpus/en_train --corpus
-\
-~/corpus/news-commentary-v8.fr-en.tok.en
-~/mosesdecoder/scripts/recaser/train-truecaser.perl \
---model ~/corpus/hi_train --corpus
-\
-~/corpus/hi-en.tok
